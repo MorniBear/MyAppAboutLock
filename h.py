3e70cb2a3e03dff761ec047997d2b44fc17bba3a
MorniBear <32993128+MorniBear@users.noreply.github.com>
2018-06-17 14:56:27 +0800

    1
    1

--- h.py
+++ h.py
@@ -1,8 +1,10 @@
 from urllib import request
 
-with request.urlopen('http://47.100.50.103/index') as f:
+with request.urlopen('http://127.0.0.1:8000/') as f:
     data = f.read()
     print('Status:', f.status, f.reason)
     for k, v in f.getheaders():
         print('%s: %s' % (k, v))
     print('Data:', data.decode('utf-8'))
+    if data.decode('utf-8') is '1':
+        print("1")
